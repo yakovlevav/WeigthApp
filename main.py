9173ed9bbf84c9f4da158277e21b3f814fefe00d
Aleksandr Yakovlev <aleksandr.yakovlev@is-it-fresh.com>
2023-05-20 20:34:01 +0200

    Added streamlit filter to the day

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
+import datetime
+
 st.title('Weight calculator')
+
+d = st.date_input(
+    "Start date",
+    datetime.date(2023, 1, 1))
 
 df_weights = pd.read_csv('weight.csv', 
                    sep=';', 
                    parse_dates = [0], 
                    names=['Date', 'Weight'], 
-                   index_col='Date', 
+                   index_col='Date',
                    decimal=',').dropna()
 
 df_weights['week'] = df_weights.index.strftime('%yw%V')
 df_weights['Weight'] = df_weights['Weight'].astype('float')
 df_weights[ 'roll' ] = df_weights.Weight.rolling(7).mean()
+
+
+df_weights = df_weights[df_weights.index.date > d]
+
+
 m_weight = pd.DataFrame()
 m_weight['Weight'] = df_weights.groupby('week').describe()['Weight']['mean']
-fig, ax = plt.subplots(figsize = (10,6))
-rs = sns.boxplot(x="week", y="Weight", data=df_weights, ax=ax)
-plt.xticks(rotation=45)
 
-st.pyplot(fig)
+fig = px.box(df_weights, x="week", y="Weight")
+
+st.plotly_chart(fig)
